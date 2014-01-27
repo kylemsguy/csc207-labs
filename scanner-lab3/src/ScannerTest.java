d211d5bf42cc1f49a2d7af302ced60fae9255282
Kyle Zhou <kylezhou2002@hotmail.com>
2014-01-27 14:28:12 -0500

    Added stuff from lab3

---
+++ scanner-lab3/src/ScannerTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		while (!input.equals("")){
+			System.out.println(input);
+			input = sc.nextLine();
+		}
+
+	}
+
+}
